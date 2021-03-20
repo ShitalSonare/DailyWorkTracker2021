805f1e0ebf6f94a5f11c7f93eb25af22ebc4986f
Sonare <shital.sonare@klm.com>
2021-03-20 09:47:00 +0100

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DailyWorkTracker.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
